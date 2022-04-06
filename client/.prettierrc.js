bfbea824f80ba87f27421cd1bc9f8c5169d41415
youjung <dbwjdrkddls1@gmail.com>
2022-04-06 15:30:43 +0900

    초기세팅

---
+++ client/.prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    singleQuote: true,
+    // 문자열은 singleQuote로 ("" -> '')
+    semi: true,
+     //코드 마지막에 세미콜론이 있게 formatting
+    useTabs: true,
+     // 들여쓰기 너비는 2칸
+    trailingComma: 'all',
+    printWidth: 80,
+    // 코드 한줄이 maximum 80칸
+    arrowParens: 'always',
+    // 화살표 함수가 하나의 매개변수를 받을 때 괄호를 생략하지 않음
+  };
