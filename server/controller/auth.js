4bc856405546b38b6d37a6bdfe5ae2d974c74967
cptkuk91 <cptkuk91@gmail.com>
2022-04-07 17:27:40 +0900

    추가: module 수정: controller/auth.js router/auth.js

--- server/controller/auth.js
+++ server/controller/auth.js
@@ -1,14 +1,43 @@
+const jwt = require("jsonwebtoken");
+const bcrypto = require("bcrypt");
+
 module.exports = {
+  login: (req, res) => {
+    res.status(200).send("login page");
+  },
 
-    login : (req,res) => {
-        res.status(200).send('login page');
-    },
+  logout: (req, res) => {
+    res.status(200).send("logout page");
+  },
 
-    logout : (req,res) => {
-        res.status(200).send('logout page');
-    },
+  signup: async (req, res) => {
+    const { email, nickname, password } = req.body;
 
-    googleLogin : (req,res) => {
+    // 이메일이 존재하는 경우, 400 Error 반환하기
+    try {
+      if (!email) {
+        // 비밀번호 암호화를 위한 bcrypt
+        const hashed = bcrypto.hashSync(password, 10);
+        await User.create({
+          //   User Model 생성하기
+          email,
+          nickname,
+          password: hashed,
+        });
 
+        const userId = User.id;
+        const result = { userId, email, nickname, password };
+
+        return res
+          .status(201)
+          .json({ data: result, message: "회원가입이 완료되었습니다." });
+      } else {
+        return res.status(400).json({ message: "이미 존재하는 이메일입니다." });
+      }
+    } catch (err) {
+      res.status(500).json({ message: "서버 에러" });
     }
-}
+  },
+
+  googleLogin: (req, res) => {},
+};
--- server/router/auth.js
+++ server/router/auth.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
+// .Router() 과연 필요한가 질문
 const authController = require('../controller/auth');
 
-router.get('/login', authController.login);
-router.get('/logout', authController.logout);
+router.post('/login', authController.login);
+router.post('/logout', authController.logout);
+router.post('/signup', authController.signup);
 
 module.exports = router;
